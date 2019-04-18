18dfa841e6953650344d62bdd1c30b6469ac203b
Maxim Blizhin <48832564+blizhinm@users.noreply.github.com>
2019-04-18 23:43:18 +0300

    Update server.js

--- testovoe-motmom/server.js
+++ testovoe-motmom/server.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 
 // иницализация express
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 // запуск сервера
 app.use(express.static('public'));
-app.listen(process.env.port || PORT, () => {
+app.listen(PORT ,() => {
 	console.log(`Server running on port ${PORT}`);
 });
 
